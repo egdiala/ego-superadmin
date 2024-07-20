7c6fe31b83c4fc60f0b47dc86efdbdf74f163128
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-07-20 17:10:59 +0100

    fix: build issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Customers, PaymentValue, ServiceRequests, TotalTrips, TripDetails, Vehicles } from "@/components/pages/dashboard";
 
-export const DashboardPage = () => {
+export const DashboardPage: React.FC = () => {
     return (
         <div className="flex flex-col gap-3.5">
             <h1 className="text-grey-dark-1 font-bold text-2xl md:text-[2rem]">Dashboard</h1>
