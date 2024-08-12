9834b8378a0ad4fce71d91f155ce0aada0e01ce8
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-08-12 15:02:32 +0100

    fix: build issues

--- src/pages/customers/customer/Dashboard.tsx
+++ src/pages/customers/customer/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const CustomerDashboardPage = () => {
+export const CustomerDashboardPage: React.FC = () => {
     return (
         <div>Customer Dashboard</div>
     )
